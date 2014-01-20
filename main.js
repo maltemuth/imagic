f854d10152eff961e5196c40521909f9bcf2a03b
maltemuth <malte.muth@googlemail.com>
2014-01-20 21:49:54 +0100

    now with touch

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ $(function() {
   $('.magic').on('mousemove', function(e) {
     b = Math.round(e.offsetX / $(window).width() * 255);
     a = Math.round(e.offsetY / $(window).height() * 255);
-    $('.magic').css("background", "rgb(" + a + ", " + b + ", 255)");
+    $('.magic').css("background", "rgb(" + a + ", " + b + ", "+ (255-a) + ")");
   });
 });
+
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+    var touch = e.touches[0];
+    //alert(touch.pageX + " - " + touch.pageY);
+    b = Math.round(touch.pageX / $(window).width() * 255);
+    a = Math.round(touch.pageY / $(window).height() * 255);
+
+    $('.magic').css("background", "rgb(" + a + ", " + b + ", "+ (255-a) + ")");
+}, false);
+
+$(window).on("resize", function() {
+  w = $(window).width();
+  h = $(window).height();
+
+  m = Math.min(w, h);
+
+  $('.magic img').width(0.9 * m);
+  $('.magic img').height(0.9 * m);
+  
+  $('.magic img').css('margin-top', -0.45 * m);
+});
+
+$(window).trigger("resize");
