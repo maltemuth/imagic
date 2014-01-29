b41005f19f634c6b8e67390aaa3abab521164916
Malte Muth <maltemuth@Maltes-MacBook-Pro.local>
2014-01-29 17:51:58 +0100

    now with hearts!

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ $(function() {
   $('.magic').on('mousemove', function(e) {
     b = Math.round(e.offsetX / $(window).width() * 255);
     a = Math.round(e.offsetY / $(window).height() * 255);
-    $('.magic').css("background", "rgb(" + a + ", " + b + ", "+ (255-a) + ")");
+    // $('.magic').css("background", "rgb(" + 128 + (a/2) + ", " + 128+ (b/2) + ", "+ ((510-a-b)/4) + ")");
+    $('.magic').css("background", "rgb(" + a + ", " + b + ", "+ (256 - (a+b)/2) + ")");
+
+    $('.magic .hearts .one').css("color", "rgb(" + (255-(a/2)) + ", " + b + ", "+ (256 - (a+b)/2) + ")");
+    $('.magic .hearts .two').css("color", "rgb(" + a + ", " + (255-(b/2)) + ", "+ (256 - (a+b)/2) + ")");
+    $('.magic .hearts .three').css("color", "rgb(" + (255-(a/2)) + ", " + (255-(b/2)) + ", "+ (256 - (a+b)/2) + ")");
+    $('.magic .hearts .four').css("color", "rgb(" + (a+b)/2 + ", " + (128 + (a-b))/2 + ", "+ (256 - (a+b)/2) + ")");
   });
 });
 
@@ -13,7 +19,7 @@ document.addEventListener('touchmove', function(e) {
     b = Math.round(touch.pageX / $(window).width() * 255);
     a = Math.round(touch.pageY / $(window).height() * 255);
 
-    $('.magic').css("background", "rgb(" + a + ", " + b + ", "+ (255-a) + ")");
+    $('.magic').css("background", "rgb(" + a + ", " + b + ", "+ ((510-a-b)/2)  + ")");
 }, false);
 
 $(window).on("resize", function() {
@@ -22,10 +28,16 @@ $(window).on("resize", function() {
 
   m = Math.min(w, h);
 
-  $('.magic img').width(0.9 * m);
-  $('.magic img').height(0.9 * m);
+  /*
+    $('.magic img').width(0.9 * m);
+    $('.magic img').height(0.9 * m);
+    
+    $('.magic img').css('margin-top', -0.45 * m);
+  */
+
+  $('.magic .hearts').css('font-size', 0.5 * m);
+
   
-  $('.magic img').css('margin-top', -0.45 * m);
 });
 
 $(window).trigger("resize");
